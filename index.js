fa45a1487f913fc6d814e29d330e74ae7e3eb13b
Arne Schlüter <aesthaddicts@gmail.com>
2016-11-23 17:57:10 +0100

    Add routes to create a puppy and to retrieve a single puppy

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const { PORT, MONGODB_URL } = process.env
 // Set up our database connection and model
 
 const mongoose = require('mongoose')
+mongoose.Promise = global.Promise
 mongoose.connect(MONGODB_URL, _ => console.log(`Mongoose connected to ${MONGODB_URL}`))
 
 const Puppy = mongoose.model('Puppy', {
@@ -25,10 +26,29 @@ const Puppy = mongoose.model('Puppy', {
 const express = require('express')
 const app = express()
 
+// Enable app to parse JSON or FORM bodies in POST / PUT / PATCH requests
+
+const bodyParser = require('body-parser')
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: false }))
+
 // Define our routes
 
 app.get('/puppies', (req, res) =>
   Puppy.find()
+    .then(data => res.json(data))
+    .catch(err => res.status(500).json(err))
+)
+
+app.post('/puppies', (req, res) => {
+  const { name, picture } = req.body
+  new Puppy({ name, picture }).save()
+    .then(pup => res.status(201).json(pup))
+    .catch(err => res.status(500).json(err))
+})
+
+app.get('/puppies/:id', (req, res) =>
+  Puppy.findById(req.params.id)
     .then(data => res.json(data))
     .catch(err => res.json(err))
 )
