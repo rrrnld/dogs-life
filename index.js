d51f413475d911daea461473cd610ff31640f4e4
Arne Schlüter <aesthaddicts@gmail.com>
2016-11-23 18:09:56 +0100

    Implement PUT /puppies/:id/likes

--- index.js
+++ index.js
@@ -37,21 +37,44 @@ app.use(bodyParser.urlencoded({ extended: false }))
 app.get('/puppies', (req, res) =>
   Puppy.find()
     .then(data => res.json(data))
-    .catch(err => res.status(500).json(err))
+    .catch(err => res.status(500).json({ message: err.message }))
 )
 
 app.post('/puppies', (req, res) => {
   const { name, picture } = req.body
   new Puppy({ name, picture }).save()
     .then(pup => res.status(201).json(pup))
-    .catch(err => res.status(500).json(err))
+    .catch(err => res.status(500).json({ message: err.message }))
 })
 
 app.get('/puppies/:id', (req, res) =>
   Puppy.findById(req.params.id)
-    .then(data => res.json(data))
-    .catch(err => res.json(err))
+    .then(pup => {
+      if (pup == null) return res.status(404).json({ message: 'Puppy not found' })
+      res.json(pup)
+    })
+    .catch(err => res.status(500).json({ message: err.message }))
 )
+
+app.put('/puppies/:id/likes', (req, res) => {
+  const { comment } = req.body
+  const like = { comment }
+
+  Puppy.findById(req.params.id)
+    .then(pup => {
+      if (pup == null) return res.status(404).json({ message: 'Puppy not found' })
+
+      // make sure the puppy doesn't get liked multiple times by the same person
+      if (pup.likes.some(like => like.ip === req.ip)) throw new Error('You already liked this puppy')
+
+      // otherwise count the like and return it
+      like.ip = req.ip
+      pup.likes.push(like)
+      return pup.save()
+    })
+    .then(pup => res.status(201).json(like))
+    .catch(err => res.status(500).json({ message: err.message }))
+})
 
 // Start the server if the app is not `require`d
 
